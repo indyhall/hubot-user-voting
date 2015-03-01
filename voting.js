d6bcd8888c78815439a81f141bec5b1ffc2067a4
Chris Morrell <inxilpro@users.noreply.github.com>
2015-02-28 19:46:09 -0500

    Moved responses into its own JSON file
    …should make user contribution a little easier

--- voting.js
+++ voting.js
@@ -25,57 +25,7 @@ var triggers =  {
 	]
 };
 
-var responses = {
-	newVotingStarted: [
-		'OK, boss.',
-		'Got it. New voting started!',
-		'Fine. I was going to watch Michael Jordan clips on YouTube, but I suppose it can wait...',
-		'There are a million unsolved questions in the universe, and you want me to spend my processing power on tallying your votes? Got it.',
-		'OK. Oh, one million votes for me? I win? Voting done!'
-	],
-	confirmClearVotes: [
-		'Are you sure you want to clear all past votes and start a new poll?'
-	],
-	cannotFindUser: [
-		'I can\'t find anyone named %s',
-		'%s? No one \'round these parts by that name...',
-	],
-	votingForSelf: [
-		'Bad form, dude.',
-		'Got it. Removing one vote for you!',
-		'Nope.',
-		'You\'re just not as cool as you think your are.',
-		'The president can vote for him or herself. You, my friend, cannot. One more reason you are not the president. One of many. (Don\'t think through that logic too much.)'
-	],
-	changingVoteTo: [
-		'Changing your vote to %s!',
-		'Fine. %s it is! You sure this time?',
-		'OK, John Kerry...',
-		'Good. I forgot to write down your vote last time. This time I\'m paying close attent... OOH YOUTUBE!'
-	],
-	ditto: [
-		'How original. %s it is...'
-	],
-	votingFor: [
-		'Got your vote for %s...',
-		'Hmm. OK. %s it is...',
-		'Hrm. Was about to post a calming manatee meme, but I guess I\'ll log your vote for %s instead.',
-		'%s? Really? I suppose there\'s no accounting for taste...',
-		'%s. Got it.',
-		'%s... let\'s see... OK... Totally...',
-		'Wow. Just wow. Did not see that one coming! %s it is.',
-		'Sure! %s! I totally love them too! I\'m so upbeat and personable! Totally not just a robot! Could a robot be so upbeat and personable!?',
-		'100100101110010101101110... Oops. Let me try that again. `System.logVoteFor("%s");` Wait. Is this where I\'m supposed to write that? I guess we\'ll find out.',
-		'What I\'m hearing is that you want to vote for %s. What I would like to hear is that you want to vote for me.',
-		'VOTE FOR......... %s!'
-	],
-	clarifyVote: [
-		'Could you be a little clearer?  One of: %s'
-	],
-	noVotes: [
-		'There are\'t any votes yet!'
-	]
-};
+var responses = require('responses.json');
 
 // Hubot Script
 module.exports = function(robot) {
